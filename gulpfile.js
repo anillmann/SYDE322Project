d6556db05f0a632cddfa4359dd479e0a625c7198
Andrew Illmann <aillmann@Andrews-MacBook-Pro.local>
2016-03-03 00:23:14 -0500

    node.js environment setup

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var jade = require('gulp-jade');
+ 
+gulp.task('templates', function () {
+	var YOUR_LOCALS = {
+		development: true,
+	};
+//Jade transfer
+	gulp.src('userApp/views/*.jade')
+	.pipe(jade({
+		locals: YOUR_LOCALS
+	}))
+	.pipe(gulp.dest('nativeUserApp/www/'));
+
+//CSS transfer
+	gulp.src('userApp/public/stylesheets/*.css')
+	.pipe(gulp.dest('nativeUserApp/www/stylesheets'));
+
+//Images transfer
+	gulp.src('userApp/public/images/*')
+	.pipe(gulp.dest('nativeUserApp/www/images'));
+
+//Javascripts transfer
+	gulp.src(['userApp/public/javascripts/*','!userApp/public/javascripts/NFC.js','!userApp/public/javascripts/nfc_utils.js'])
+	.pipe(gulp.dest('nativeUserApp/www/javascripts'));
+
+});
