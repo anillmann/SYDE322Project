a01ddb7d151e5acfc15c58881575df8ca28f0507
melissalynett1 <melissa.a.lynett@gmail.com>
2016-03-25 18:24:44 -0400

    Added update company form

--- traderPRO/public/javascripts/metadatamgmt.js
+++ traderPRO/public/javascripts/metadatamgmt.js
@@ -30,4 +30,33 @@ $(document).ready( function() {
 		});
 	});
 
+		// update industry list when sector updated
+	$('#update-company-field-sector').change( function () {
+		var sectorId = $('#update-company-field-sector select').val();
+		var sqlParams = JSON.stringify({
+			'todo' : 'getIndustries',
+			'sqlParams' : {
+				'sectorId' : sectorId
+			}
+		});	
+
+		$.ajax('metadatamgmt',{
+			type : 'POST',
+			contentType : 'application/json',
+			dataType : 'JSON',
+			data : sqlParams,
+			success : function (results) {
+				var industry = [], industries = [];
+				for (x in results) {
+					industry = [results[x].industryId, results[x].industry];
+					industries.push(industry);
+				}
+				var industrySelect = $('#update-company-field-industry select');
+				appendList(industrySelect,'option',industries);
+				$('#update-company-field-industry').show();
+			}
+		});
+	});
+
+
 });
